5aa4404760099b6c5e69a99c9732f88298b1171f
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-16 21:44:42 +0000

    Started socket server in python main

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# 
+from socket import *
+## Create socket - the endpoint of a link between programs running over a network
+host = "127.0.0.1"
+port = 5000
+
+# Accept connection
+s = socket()
+s.bind((host, port)) # bind the socket to the address and port to listen to 
+print("Searching!")
+s.listen(1) # socket is now listening for new connections - if 1 connection is refused, the socket will refuse all future connections
+
+conn, addr = s.accept()
+print(f"Connection from {addr}")
+# When it recieves a message (chance of rain), print it
